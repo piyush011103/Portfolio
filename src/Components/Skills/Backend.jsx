bc2504a296729624eebdfbe5377d76a611a5ad33
Piyush Kumar <154053633+piyush011103@users.noreply.github.com>
2025-07-03 21:42:55 +0530

    Update Backend.jsx

--- src/Components/Skills/Backend.jsx
+++ src/Components/Skills/Backend.jsx
@@ -30,7 +30,7 @@ const Backend = () => {
                       className="skills_progress"
                       style={{ width: `${skill.level}%` }}
                     ></div>
-                    <span className="skills_level">{skill.level}%</span>
+{/*                     <span className="skills_level">{skill.level}%</span> */}
                   </div>
                 </div>
               </div>
